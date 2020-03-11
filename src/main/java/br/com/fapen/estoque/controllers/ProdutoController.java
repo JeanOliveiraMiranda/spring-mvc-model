b717f47825176578cc8fd43e8492e10c6d4adb90
JeanOliveiraMiranda <mirandajean2009@gmail.com>
2020-03-11 01:48:21 -0300

    adicionando CRUD no controller de Produto

--- src/main/java/br/com/fapen/estoque/controllers/ProdutoController.java
+++ src/main/java/br/com/fapen/estoque/controllers/ProdutoController.java
@@ -2,13 +2,15 @@ package br.com.fapen.estoque.controllers;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,38 @@ public class ProdutoController {
     public Produto getById(@PathVariable Long id) {
         Optional<Produto> produto = produtoRepository.findById(id);
         return produto.orElseThrow(() -> new DataNotFoundException("Produto Not Found"));
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> post(@RequestBody Produto model) {
+
+        Produto produto = produtoRepository.save(model);
+
+        return ResponseEntity.ok(produto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public Produto put(@PathVariable Long id, @RequestBody Produto model) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if (produto.isPresent()) {
+            model.setIdProduto(id);
+            produtoRepository.save(model);
+        }
+
+        return produto.orElseThrow(() -> new DataNotFoundException("Produto Not found"));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Produto delete(@PathVariable Long id) {
+
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        // erro 500 quando não encontra o cliente
+        if (produto.isPresent()) {
+            produtoRepository.deleteById(id);
+        }
+
+        return produto.orElseThrow(() -> new DataNotFoundException("Produto Not found"));
     }
 }
